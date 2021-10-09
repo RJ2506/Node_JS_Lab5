2566373a474024fd837a50c44e49cd75df34cd17
Rodolf John Gayem <rjgayem@gmail.com>
2021-10-08 19:10:46 -0700

    finish createPost

--- Lab5(group)/blog.js
+++ Lab5(group)/blog.js
@@ -1,13 +1,20 @@
 const fs = require("fs");
-const process = require("process");
 const path = require("path");
-let arg = process.argv;
-let user_account = `username: ${arg[2]} password: ${arg[3]}`;
-let blog_name = arg[4];
 
+let user_account = `username: RJ password: RJ25`;
+let blog_name = "RJ_Blog";
+
+let title = "super mario";
+let content = `
+like: 1
+like by: you
+
+jump mario jump save the princess
+sup bowser
+`;
 let folder_path = path.join(__dirname, blog_name);
+let post_folder = path.join(__dirname, "RJ_Blog");
 const FILE_NAME = "database.txt";
-const FILE_PATH = path.join(__dirname, folder_path, FILE_NAME);
 
 // register(username,password)
 const fsReadfileP = (file) => {
@@ -56,7 +63,44 @@ const createABlog = (blogName) => {
 };
 
 // createPost(postTitle, postContent, blogName)
-const createPost = (postTitle, postContent, blogName) => {};
+const createPost = (postTitle, postContent, blogName) => {
+    return new Promise((resolve, reject) => {
+        fs.readdir(blogName, (err, file) => {
+            if (err) {
+                reject(new Error(`${blogName} doesn't exists`));
+            } else {
+                if (file.includes(`${postTitle.replace(" ", "_")}.txt`)) {
+                    //make space into  _ then add +1 at the end
+                    fs.writeFile(
+                        `${path.join(blogName, postTitle.replace(" ", "_"))}1.txt`,
+                        postContent,
+                        (err) => {
+                            if (err) {
+                                reject(err);
+                            } else {
+                                resolve();
+                            }
+                        }
+                    );
+                } else {
+                    // make space into _ of the title
+                    // write / post a content
+                    fs.writeFile(
+                        `${path.join(blogName, postTitle.replace(" ", "_"))}.txt`,
+                        postContent,
+                        (err) => {
+                            if (err) {
+                                reject(err);
+                            } else {
+                                resolve();
+                            }
+                        }
+                    );
+                }
+            }
+        });
+    });
+};
 
 // likePost(blogName, postTitle, username)
 
@@ -69,5 +113,8 @@ fsReadfileP(FILE_NAME)
         createABlog(folder_path);
         console.log("Blog has been created");
     })
-    .then((blogName) => createPost(title, content, blogName))
+    .then(() => {
+        createPost(title, content, post_folder);
+        console.log("Post created");
+    })
     .catch((err) => console.log(err.message));
